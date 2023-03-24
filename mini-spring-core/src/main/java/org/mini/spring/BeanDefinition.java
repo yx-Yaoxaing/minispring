fbe0a0dd3c2b5a937cf9cefdf6a2a20c697495e7
‘yaoxiang <yaoxiang@cqliving.com>
2023-03-24 09:28:33 +0800

    BeanDefinition中添加Bean的定义信息，初始化方法 class路径名以及ArgumentValues的解析存储和scope作用域(默认单例)

--- mini-spring-core/src/main/java/org/mini/spring/BeanDefinition.java
+++ mini-spring-core/src/main/java/org/mini/spring/BeanDefinition.java
@@ -3,9 +3,26 @@ package org.mini.spring;
 
 public class BeanDefinition {
 
+    String SCOPE_SINGLETON = "singleton";
+    String SCOPE_PROTOTYPE = "prototype";
+
     private String id;
 
     private String className;
+
+    // 默认为单例模式
+    private String scope = SCOPE_SINGLETON;
+
+    private volatile Object beanClass;
+
+    // 初始化方法
+    private String initMethod;
+
+    private ArgumentValues argumentValues;
+
+    private boolean lazyInit = false;
+
+
 
     public BeanDefinition(String id, String className) {
         this.id = id;
